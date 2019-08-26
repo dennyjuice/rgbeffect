2bffeb51098daf582ca152eff6d97a7677d47f51
Denis Samborsky <dennyjuice@gmail.com>
2019-08-26 22:12:12 +0700

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+def stylize(offset, image):
+    image = Image.open(image)
+
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+
+    red, green, blue = image.split()
+    red = red.crop((offset, 0, red.width, red.height))
+    blue = blue.crop((0, 0, blue.width - offset, blue.height))
+    green = green.crop((offset / 2, 0, green.width - offset / 2, green.height))
+
+    stylize_image = Image.merge("RGB", (red, green, blue))
+    stylize_image.save("new_avatar.jpg")
+
+if __name__ == "__main__":
+    stylize(50, 'monro.jpg')
